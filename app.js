0135f74a6b9384eec573d8562429d5b2657ed923
Darsan Dinesh <darsandinesh100@gmail.com>
2024-06-29 23:07:44 +0530

    console message added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    console.log('update in app.js')
+    console.log('update in app.js!')
     res.send('server!!!!')
 })
 
