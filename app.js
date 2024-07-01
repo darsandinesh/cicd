7f711aa64bd1e91fe3ac21e21815cd6deb6366d4
Darsan Dinesh <darsandinesh100@gmail.com>
2024-07-01 11:00:04 +0530

    new message added and console removed

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    console.log('update in app.js!')
-    res.send('i am darsan dinesh this is my server!')
+    res.send('Hello server here...')
 })
 
 app.listen(1111,()=>console.log('server started'))
