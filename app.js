dca57fe2787f9bb323b128f8b715a18551e557cd
Darsan Dinesh <darsandinesh100@gmail.com>
2024-07-26 21:48:03 +0530

    html page updated

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('Hello server')
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(1111,()=>console.log('server started'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
